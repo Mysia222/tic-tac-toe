cfdacf8bdd2836154c5323972929a337da537e65
Yulia <milkiway897@gmail.com>
2017-01-31 14:59:10 +0300

    next turn

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -12,10 +12,17 @@ this.play='x';
 
     getCurrentPlayerSymbol() {
 
+return this.play;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+    	 if (this.getFieldValue(rowIndex, columnIndex) === null) {
+        this.matrix[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+     	if(this.play==='x')
+     		this.play='o';
+     	if(this.play==='o')
+     		this.play='x';
+     }
     }
 
     isFinished() {
