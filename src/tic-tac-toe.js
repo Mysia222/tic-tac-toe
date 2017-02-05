a9be8abf7da8bb0a4f0a6e0c9932d10e81679de3
Yulia <milkiway897@gmail.com>
2017-02-05 21:43:28 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,43 +1,67 @@
 class TicTacToe {
     constructor() {
 this.matrix=[
-  ['','',''],
-  ['','',''],
-  ['','',''],
+  [null,null,null],
+  [null,null,null],
+  [null,null,null],
 ];
-//this.rowIndex='';
-//this.colIndex='';
-this.play='x';
+
+this.player='x';
     }
 
     getCurrentPlayerSymbol() {
 
-return this.play;
+return this.player;
     }
 
     nextTurn(rowIndex, columnIndex) {
-    	 if (this.getFieldValue(rowIndex, columnIndex) === '') {
+    	if (this.getFieldValue(rowIndex, columnIndex) == null) 
+    	{
        this.matrix[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
-     	if(this.play==='x')
-     	this.play='o';
-     	if(this.play==='o')
-     	this.play='x';
+     	if(this.player=='x')
+     	this.player ='o';
+ else
+     	this.player ='x';
      }
+     else
+     	return this;
     }
 
     isFinished() {
-      
+    	
+      	if(this.getWinner()!=null || this.isDraw() )
+      	{
+      		return true;
+      	}
+      	else
+      		return false;
+
     }
 
     getWinner() {
+    	/*if(this.matrix[0][0]=='x' && this.matrix[0][1]=='x' && this.matrix[0][2]=='x' ) return 'x';
+    	if(this.matrix[0][0]=='x' && this.matrix[1][0]=='x' && this.matrix[2][0]=='x' ) return 'x';
+    	if(this.matrix[0][0]=='x' && this.matrix[1][1]=='x' && this.matrix[2][2]=='x' ) return 'x';
+    	if(this.matrix[0][2]=='x' && this.matrix[1][2]=='x' && this.matrix[2][2]=='x' ) return 'x';
+    	if(this.matrix[0][1]=='x' && this.matrix[1][1]=='x' && this.matrix[2][1]=='x' ) return 'x';
+    	if(this.matrix[2][0]=='x' && this.matrix[2][1]=='x' && this.matrix[2][2]=='x' ) return 'x';
 
-
+    	if(this.matrix[0][0]=='o' && this.matrix[0][1]=='o' && this.matrix[0][2]=='o' ) return 'o';
+    	if(this.matrix[0][0]=='o' && this.matrix[1][0]=='o' && this.matrix[2][0]=='o' ) return 'o';
+    	if(this.matrix[0][0]=='o' && this.matrix[1][1]=='o' && this.matrix[2][2]=='o' ) return 'o';
+    	if(this.matrix[0][2]=='o' && this.matrix[1][2]=='o' && this.matrix[2][2]=='o' ) return 'o';
+    	if(this.matrix[0][1]=='o' && this.matrix[1][1]=='o' && this.matrix[2][1]=='o' ) return 'o';
+    	if(this.matrix[2][0]=='o' && this.matrix[2][1]=='o' && this.matrix[2][2]=='o' ) return 'o';
+    
+    	return null;
+    
+    */
     }
 
     noMoreTurns() {
         for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
-          if (this.matrix[i][j] === '') 
+          if (this.matrix[i][j] === null) 
           	return false;
         }
       }
@@ -45,14 +69,20 @@ return this.play;
     }
 
     isDraw() {
-
+    if(noMoreTurns())
+    	return true;
+    else
+    	return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
+	
+    if(rowIndex<3 && colIndex<3)
+    		return this.matrix[rowIndex][colIndex];
+    else 
+    	return null;
+}
 
-    	return this.matrix[rowIndex][colIndex];
-
-    }
 }
 
 module.exports = TicTacToe;
